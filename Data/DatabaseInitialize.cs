f05c4241f2c6087aae5f6bff1ecef45edbcc4d51
nmq126 <minhquangngo.98@gmail.com>
2022-02-19 20:02:46 +0700

    push

---
+++ Data/DatabaseInitialize.cs
@@ -0,0 +1,59 @@
+﻿using Exam.Entity;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Data
+{
+    class DatabaseInitialize
+    {
+        private static readonly SQLiteConnection cnn = new SQLiteConnection("uwpexam.db");
+        public static bool CreateTables()
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS
+                          Contact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                    Name    VARCHAR(255),
+                                    PhoneNumber    VARCHAR(255) UNIQUE
+                                    );";
+            using (var statement = cnn.Prepare(sql))
+            {
+                statement.Step();
+            }
+            return true;
+        }
+        public static bool InsertContact(Contact contact)
+        {
+            using (var stt = cnn.Prepare("INSERT INTO Contact(Name, PhoneNumber) VALUES (?, ?)"))
+            {
+                stt.Bind(1, contact.Name);
+                stt.Bind(2, contact.PhoneNumber);
+                stt.Step();
+            }
+            return true;
+        }
+        public static List<Contact> ShowListContact()
+        {
+            List<Contact> list = new List<Contact>();
+            using (var stt = cnn.Prepare("select * from Contact"))
+            {
+
+                while (stt.Step() == SQLiteResult.ROW)
+                {
+
+                    Contact contact = new Contact()
+                    {
+                        Id = Convert.ToInt32(stt["Id"]),
+                        Name = (string)stt["Name"],
+                        PhoneNumber = (string)stt["PhoneNumber"]
+                    };
+                    
+                    list.Add(contact);
+                }
+                return list;
+            }
+        }
+    }
+}
---
+++ Pages/CreateContactPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Exam.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Exam.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CreateContactPage : Page
+    {
+        private int validateCheck;
+        public CreateContactPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateForm())
+            {
+                return;
+            }
+            Contact contact = new Contact
+            {
+                Name = txtName.Text,
+                PhoneNumber = txtPhone.Text,
+            };
+            ContentDialog contentDialog = new ContentDialog();
+            if (Data.DatabaseInitialize.InsertContact(contact))
+            {
+                contentDialog.Title = "Action success";
+                contentDialog.Content = "Added contact successfully";
+                txtName.Text = "";
+                txtPhone.Text = "";
+            }
+            else
+            {
+                contentDialog.Title = "Action fail";
+                contentDialog.Content = "Transaction added fail";
+            }
+            contentDialog.CloseButtonText = "Close";
+            await contentDialog.ShowAsync();
+        }
+        private bool ValidateForm()
+        {
+            validateCheck = 0;
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                msgName.Text = "*Please enter name";
+                validateCheck++;
+            }
+            else
+            {
+                msgName.Text = "";
+            }
+            if (string.IsNullOrEmpty(txtPhone.Text))
+            {
+                msgPhone.Text = "*Please enter phone";
+                validateCheck++;
+            }
+            else
+            {
+                msgPhone.Text = "";
+            }
+            return validateCheck > 0;
+        }
+    }
+}
